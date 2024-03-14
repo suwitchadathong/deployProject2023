10c4c6bc450cfb31682330d1c584744de4881cca
suwitchadathong <64015155@kmitl.ac.th>
2024-03-14 13:13:49 +0700

    up

--- frontend/src/Components/SingIn.js
+++ frontend/src/Components/SingIn.js
@@ -183,7 +183,7 @@ function AppSingIn(){
               <div className='center'>
                 <GoogleLogin 
                   clientId={clientId}
-                  buttonText="Sing in with Google"
+                  buttonText="Sign in with Google"
                   onSuccess={onSuccess}
                   onFailure={onFailure}
                   cookiePolicy={"single_host_origin"}
@@ -192,7 +192,7 @@ function AppSingIn(){
               </div> 
               <form onSubmit={handleSubmit}>
                   <div className="bx-input-fix">
-                      <label htmlFor="Email">อีเมล์</label>
+                      <label htmlFor="Email">อีเมล</label>
                       <input
                           type="email"
                           id="Email"
