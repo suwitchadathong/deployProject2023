221883f10ee90527eab627849ddc96a6f9060fff
suwitchadathong <64015155@kmitl.ac.th>
2024-03-27 20:13:46 +0700

    edit

--- restframework/mysite/settings.py
+++ restframework/mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 # ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1', '161.246.4.12']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
